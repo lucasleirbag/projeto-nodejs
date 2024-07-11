c39a7e245913a9d3e4d287b7e7cdb48337380159
lucas.ribeiro <lucas.ribeiro@cebraspe.org.br>
2024-07-11 14:09:54 -0300

    feat: Adiciona conexão ao banco de dados MySQL e consulta de teste

---
+++ src/test/testConnection.js
@@ -0,0 +1,7 @@
+const connection = require('../infrastructure/database');
+
+connection.query('SELECT 1 + 1 AS solution', (error, results) => {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+  connection.end();
+});
